5e93bbce51dc19f541bc4101251706b5e2f90bf6
joshuaPhong <klx2044@yahoo.com>
2023-04-02 11:58:09 +1200

    basic sanitising

--- basix_sanitising.php
+++ basix_sanitising.php
@@ -14,11 +14,11 @@ $user_answer = "";
 $submission_response = "";
 
 // this filter will strip all but the int. e.g. <p>-5</p> will be sanitised to -5
-if ($_SERVER['REQUEST_METHOD'] === "POST"){
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
     $user_answer = filter_var($_POST["answer"], FILTER_SANITIZE_NUMBER_INT);
-    if ($user_answer == -5){
+    if ($user_answer == -5) {
         $submission_response = "Correct!";
-    }else{
+    } else {
         $validation_error = "* Wrong answer. Try again.";
     }
 }
@@ -28,13 +28,97 @@ if ($_SERVER['REQUEST_METHOD'] === "POST"){
 <form method="post" action="">
     <h4>Question 1:</h4>
     <p>What is 6 - 11?</p>
-    <input type="text" name="answer" id="answer" value="<?= $user_answer;?>">
+    <input type="text" name="answer" id="answer" value="<?= $user_answer; ?>">
     <br>
-    <span class="error" id="error"><?= $validation_error;?></span>
+    <span class="error" id="error"><?= $validation_error; ?></span>
     <br>
     <input type="submit" value="Submit Your Answer">
 </form>
 <div>
-    <p id="answer-display">Your answer was: <?= $user_answer;?></p>
-    <p id="submission-response"><?= $submission_response;?></p>
+    <p id="answer-display">Your answer was: <?= $user_answer; ?></p>
+    <p id="submission-response"><?= $submission_response; ?></p>
 </div>
+
+<?php
+$validation_error = "";
+$user_url = "";
+$form_message = "";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $user_url = $_POST["url"];
+    
+    if (filter_var($user_url, FILTER_SANITIZE_URL)) {
+        
+        $validation_error = "* This is an invalid URL";
+        $form_message = "Please retry and submit your form again";
+    } else {
+        $form_message = "Thank you for your submission";
+    }
+}
+?>
+
+<form method="post" action="">
+    Your Favorite Website:
+    <br>
+    <input type="text" name="url" value="<?php echo $user_url; ?>">
+    <span class="error"><?= $validation_error; ?></span>
+    <br>
+    <input type="submit" value="Submit">
+</form>
+<p> <?= $form_message; ?> </p>
+<?php
+// using options with filter_var()
+
+$message = "";
+$month_error = "";
+$day_error = "";
+$year_error = "";
+
+// Create your variables here:
+$month_options = ["options" => ["min_range" => 1, "max_range" => 12]];
+
+$day_options = ["options" => ["min_range" => 1, "max_range" => 31]];
+
+$year_options = ["options" => ["min_range" => 1903, "max_range" => date("Y")]];
+
+
+// Define your function here: type = string month/day/year
+// $error. passed by reference. is the error message passed by input type
+// $options_arr. the array being passed
+function validateInput($type, &$error, $options_arr): bool
+{
+    if (!filter_var($_POST[$type], FILTER_VALIDATE_INT, $options_arr)) {
+        $error = "* Invalid $type";
+        return false;
+    } else {
+        return true;
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Uncomment the code below:
+    $test_month = validateInput("month", $month_error, $month_options);
+    $test_day = validateInput("day", $day_error, $day_options);
+    $test_year = validateInput("year", $year_error, $year_options);
+    if ($test_month && $test_day && $test_year) {
+        $message = "Your birthday is: {$_POST["month"]}/{$_POST["day"]}/{$_POST["year"]}";
+    }
+}
+
+?>
+
+<form method="post" action="">
+    Enter your birthday:
+    <br>
+    Month: <input type="number" name="month" value="<?= $_POST["month"]; ?>">
+    <span class="error"><?= $month_error; ?>		</span>
+    <br>
+    Day: <input type="number" name="day" value="<?= $_POST["day"]; ?>">
+    <span class="error"><?= $day_error; ?>		</span>
+    <br>
+    Year: <input type="number" name="year" value="<?= $_POST["year"]; ?>">
+    <span class="error"><?= $year_error; ?>		</span>
+    <br>
+    <input type="submit" value="Submit">
+</form>
+<p><?= $message; ?></p>
